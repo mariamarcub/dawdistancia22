ccef98a1f27659551daf61e797bd9415d9c943a7
mariamarcub <104356217+mariamarcub@users.noreply.github.com>
2022-04-25 12:27:06 +0200

    Add files via upload

---
+++ practica-martinez.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+*Devuelve el cuadrado del numero 4
+*
+*@author María Martínez Cubiles
+*@version 4.8 nano y 7.4 de PHP
+*@param integer $num
+*@return me devuelve el cuadrado del número seleccionado
+*{@internal Obtenemos el número 16 al ser el cuadrado del número 4}
+*/
+
+function cuadrado($num){
+	return $num * $num;
+        }
+        echo "cuadrado(4)";
+
+
+/**
+*Devuelve el resultado de la suma de dos números
+*
+*@author María Martínez Cubiles
+*@version 4.8 nano y 7.4 de PHP
+*@param $valor1 y $valor2. Son los números a sumar
+*@return me devuelve la suma de ambos números
+*{@internal Obtenemos un número tras sumar el $valor1 y el $valor2}
+*/
+
+function suma($valor1, $valor2){
+	return $valor1 + $valor2;
+}
+
+?> 
